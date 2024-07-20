4964a3bedae996429e788653fc8ef4ffd96a7329
Deepak <deepakind777@gmail.com>
2024-07-20 08:04:14 +0000

    final commit

--- src/components/ClickCounter/index.js
+++ src/components/ClickCounter/index.js
@@ -6,10 +6,10 @@ class ClickCounter extends Component {
   state = {count: 0}
 
   onIncrement = () => {
-    this.setState(prevState =>
-      /* console.log(`previous State Count = ${prevState.count}`)  */
-      ({count: prevState.count + 1}),
-    )
+    this.setState(prevState => {
+      console.log(`Previous State Count = ${prevState.count}`)
+      return {count: prevState.count + 1}
+    })
   }
 
   render() {
@@ -21,8 +21,8 @@ class ClickCounter extends Component {
           <span className="time-style">{count} </span> times{' '}
         </h1>
         <p className="description">Click the button to increase the count! </p>
-        <button className="button" onClick={this.onIncrement}>
-          Click Me!{' '}
+        <button className="button" onClick={this.onIncrement} type="submit">
+          Click Me!
         </button>
       </div>
     )
